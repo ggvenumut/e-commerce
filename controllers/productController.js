f861b6427fb33ebc6d489285b434f77ef1977ae1
umut guven <ggvenumut@gmail.com>
2022-07-27 21:45:01 +0300

    Create Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,9 @@
 import Product from "../models/Product.js";
 
 const createProduct = async (req, res) => {
-  res.send("create product");
+  req.body.user = req.user.userID;
+  const product = await Product.create(req.body);
+  res.status(200).json({ product });
 };
 const getAllProducts = async (req, res) => {
   res.send("get All Products");
